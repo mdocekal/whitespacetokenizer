b28aa5107be7851f977b267c0df4c1f2a46ddc24
Martin Dočekal <docekmartin@gmail.com>
2024-11-28 14:09:12 +0100

    sdist distribution

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf dist
 python -m pip install --upgrade build
-python -m build
+python -m build --sdist
 python -m pip install --user --upgrade twine
 python -m twine upload dist/*
